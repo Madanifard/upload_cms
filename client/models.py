c55a7a9c5a13c7770769aafaf4a820d922e0e14b
Madanifard <amadanifard@gmail.com>
2021-08-06 16:24:47 +0430

    Build client app  and models where needed

---
+++ client/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CASCADE
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Information(models.Model):
+    user_id = models.OneToOneField(User, on_delete=CASCADE, related_name='user_information')
+    national_code = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to='avatars')
+    nationality = models.CharField(max_length=50)
+    passport_code = models.CharField(max_length=50)
+
+
+class Mobiles(models.Model):
+    user_id = models.ForeignKey(User, related_name='user_mobile', on_delete=CASCADE)
+    mobile = models.CharField(max_length=30)
+    sms_code = models.CharField(max_length=10, blank=True, default=None)
+    date_sent = models.DateTimeField(blank=True, default=None)
+    count_sent = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(3)])
+    verified = models.BooleanField(default=False)
+
+
+class Address(models.Model):
+    user_id = models.ForeignKey(User, related_name='user_address', on_delete=CASCADE)
+    directions = models.CharField(max_length=151)
+    postal_code = models.CharField(max_length=50)
+    phone = models.CharField(max_length=30)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+
+class SecurityQuestions(models.Model):
+    user_id = models.ForeignKey(User, related_name='user_security_questions', on_delete=CASCADE)
+    question = models.TextField()
+    answer = models.TextField()
--- upload_cms/settings.py
+++ upload_cms/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / "uploads"
